7e220b702a9cf09de77c25580c9e02b99f9b6dcc
Ande-Lakshmi-Nishitha <nishitha.lakshmi.ande@gmail.com>
2026-04-09 22:51:01 +0530

    changed details in run_experiments.py

--- .history/experiments/run_experiments_20260409224511.py
+++ .history/experiments/run_experiments_20260409224511.py
@@ -1,6 +1,5 @@
 import numpy as np
-from qiskit_aer import Aer
-from qiskit import transpile
+from qiskit import Aer, transpile
 from qiskit.quantum_info import Statevector
 from circuits.teleportation import create_teleportation_circuit
 from noise.noise_models import create_noise_model
@@ -16,7 +15,10 @@ def run():
         fidelities=[]
 
         for _ in range(30):
-            qc=create_teleportation_circuit()
+            theta=np.random.uniform(0,np.pi)
+            phi=np.random.uniform(0,2*np.pi)
+
+            qc=create_teleportation_circuit(theta,phi)
 
             #ideal state with no noise
             ideal_sv =Statevector.from_instruction(qc)
@@ -38,3 +40,4 @@ def run():
         print(f"Noise: {p}, Avg Fidelity: {avg_fidelity:.4f}")
 
     return results
+
