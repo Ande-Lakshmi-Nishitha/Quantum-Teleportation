e43892937bc3aba9e9a7daab4ed8c3c8d5520b22
Nehaa Joshi <nehaajoshi2006@gmail.com>
2026-04-10 21:02:51 +0530

    tried to fix code

--- analysis/fidelity.py
+++ analysis/fidelity.py
@@ -1,7 +1,9 @@
-from  qiskit.quantum_info import partial_trace, state_fidelity
+from qiskit.quantum_info import partial_trace, state_fidelity
+
 
 def compute_fidelity(ideal_sv, noisy_sv):
-    ideal_reduced =partial_trace(ideal_sv,[0,1])
-    noisy_reduced = partial_trace(noisy_sv,[0,1])
+    # Keep only qubit 2 (teleported qubit)
+    ideal_reduced = partial_trace(ideal_sv, [0, 1])
+    noisy_reduced = partial_trace(noisy_sv, [0, 1])
 
-    return state_fidelity(ideal_reduced,noisy_reduced)
+    return state_fidelity(ideal_reduced, noisy_reduced)
--- experiments/run_experiments.py
+++ experiments/run_experiments.py
@@ -1,39 +1,44 @@
 import numpy as np
 from qiskit_aer import Aer
-from qiskit import transpile
-from qiskit.quantum_info import Statevector
+
 from circuits.teleportation import create_teleportation_circuit
 from noise.noise_models import create_noise_model
 from analysis.fidelity import compute_fidelity
 
+
 def run():
-    backend = Aer.get_backend('aer_simulator')
-    noise_levels = [0,0.01,0.05,0.1,0.2]
-    results=[]
+    backend = Aer.get_backend("aer_simulator")
+
+    noise_levels = [0, 0.01, 0.05, 0.1, 0.2]
+    results = []
 
     for p in noise_levels:
-        noise_model=create_noise_model(p)
-        fidelities=[]
+        noise_model = create_noise_model(p)
+        fidelities = []
 
         for _ in range(30):
-            qc=create_teleportation_circuit()
+            theta = np.random.uniform(0, np.pi)
+            phi = np.random.uniform(0, 2*np.pi)
 
-            #ideal state with no noise
-            ideal_sv =Statevector.from_instruction(qc)
+            qc = create_teleportation_circuit(theta, phi)
 
-            #simulation creation
-            qc.save_statevector()
-            tqc=transpile(qc,backend)
+            # -------- IDEAL --------
+            ideal_qc = qc.copy()
+            ideal_qc.save_statevector()
+            ideal_result = backend.run(ideal_qc).result()
+            ideal_sv = ideal_result.get_statevector()
 
-            #running with noise
-            result = backend.run(tqc,noise_model=noise_model).result()
-            noisy_sv =result.get_statevector()
+            # -------- NOISY --------
+            noisy_qc = qc.copy()
+            noisy_qc.save_statevector()
+            noisy_result = backend.run(noisy_qc, noise_model=noise_model).result()
+            noisy_sv = noisy_result.get_statevector()
 
-            fidelity=compute_fidelity(ideal_sv,noisy_sv)
+            fidelity = compute_fidelity(ideal_sv, noisy_sv)
             fidelities.append(fidelity)
 
-        avg_fidelity=np.mean(fidelities)
-        results.append((p,avg_fidelity))
+        avg_fidelity = np.mean(fidelities)
+        results.append((p, avg_fidelity))
 
         print(f"Noise: {p}, Avg Fidelity: {avg_fidelity:.4f}")
 
